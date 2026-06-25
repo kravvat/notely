9735637af1e072e8c7c20ba46a2e037b38642077
kravvat <kacperstec3d@gmail.com>
2026-06-25 17:03:05 +0200

    Brake code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,8 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return null;
+    // return null;
+    return "siema"
   }
 
   const splitAuth = authHeader.split(" ");
