be1c962ffd7321d3822b75bb9995210ade74c025
kravvat <kacperstec3d@gmail.com>
2026-06-25 17:55:09 +0200

    Break code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,8 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return null;
+    return "siema"
+    // return null;
   }
 
   const splitAuth = authHeader.split(" ");
